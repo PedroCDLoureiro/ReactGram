d66fee0ccd3807bc9d6fa22c83cb8ad01493242f
Pedro Loureiro <52416553+PedroCDLoureiro@users.noreply.github.com>
2025-02-20 00:23:32 -0300

    Validações de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -14,9 +14,80 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async (req, res) => {
-    res.send("Registro");
+    const { name, email, password } = req.body;
+
+    // check if user exists
+    const user = await User.findOne({ email });
+
+    if (user) {
+        res.status(442).json({ errors: ["Por favor, utilize outro e-mail."] });
+        return;
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash,
+    });
+
+    // If user was created successfully, return the token
+    if (!newUser) {
+        res.status(422).json({
+            errors: ["Houve um erro, por favor tente mais tarde."],
+        });
+        return;
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
+    });
+};
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    // Check if user exists
+    if (!user) {
+        res.status(404).json({ errors: ["Usuário não encontrado."] });
+        return;
+    }
+
+    // Check if password matches
+    if (!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({ errors: ["Senha inválida."] });
+        return;
+    }
+
+    // Return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id),
+    });
+};
+
+// Get current logged in user
+const getCurrentUser = async (req, res) => {
+    const user = req.user;
+
+    res.status(200).json(user);
+};
+
+// Update an user
+const update = async (req, res) => {
+    res.send("Update");
 };
 
 module.exports = {
     register,
+    login,
+    getCurrentUser,
+    update,
 };
