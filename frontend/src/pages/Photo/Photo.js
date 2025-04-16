3133e42685cd44b4f871410332eafda766a4d3a9
Pedro Loureiro <52416553+PedroCDLoureiro@users.noreply.github.com>
2025-04-16 20:02:52 -0300

    likes function

--- frontend/src/pages/Photo/Photo.js
+++ frontend/src/pages/Photo/Photo.js
@@ -6,18 +6,22 @@ import { Link } from "react-router-dom";
 // components
 import Message from "../../components/Message";
 import PhotoItem from "../../components/PhotoItem";
+import LikeContainer from "../../components/LikeContainer";
 
 // hooks
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 // redux
-import { getPhoto } from "../../slices/photoSlice";
+import { getPhoto, like } from "../../slices/photoSlice";
 
 const Photo = () => {
     const { id } = useParams();
     const dispatch = useDispatch();
+
+    const resetMessage = useResetComponentMessage(dispatch);
 
     const { user } = useSelector((state) => state.auth);
 
@@ -35,6 +39,12 @@ const Photo = () => {
 
     // like and comments
 
+    const handleLike = () => {
+        dispatch(like(photo._id));
+
+        resetMessage();
+    };
+
     if (loading) {
         return <p>Carregando...</p>;
     }
@@ -42,6 +52,11 @@ const Photo = () => {
     return (
         <div id="photo">
             <PhotoItem photo={photo} />
+            <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+            <div className="message-container">
+                {error && <Message msg={error} type="error" />}
+                {message && <Message msg={message} type="success" />}
+            </div>
         </div>
     );
 };
