a121cee5ac0d989d52a7e279e9059c0793e79705
Pedro Loureiro <52416553+PedroCDLoureiro@users.noreply.github.com>
2025-04-16 23:10:43 -0300

    search function

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -125,6 +125,21 @@ const getPhotos = async (token) => {
     }
 };
 
+// search photo by title
+const searchPhotos = async (query, token) => {
+    const config = requestConfig("GET", null, token);
+
+    try {
+        const res = await fetch(api + `/photos/search?q=${query}`, config)
+            .then((res) => res.json())
+            .catch((err) => err);
+
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 const photoService = {
     publishPhoto,
     getUserPhotos,
@@ -134,6 +149,7 @@ const photoService = {
     like,
     comment,
     getPhotos,
+    searchPhotos,
 };
 
 export default photoService;
